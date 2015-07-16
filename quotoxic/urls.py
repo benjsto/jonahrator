a2271d74d1898f9b57ecb501de577fccfb4f12ad
Benjamin Stockwell <benjamin.stockwell@buzzfeed.com>
2015-07-16 17:26:08 -0500

    Add favicon.

--- quotoxic/urls.py
+++ quotoxic/urls.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
-from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
+from django.conf.urls import patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from .views import (
@@ -12,6 +13,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^quote/$', QuoteView.as_view(), name='quote'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/public/images/favicon.ico'))
 )
 
 urlpatterns += staticfiles_urlpatterns()
